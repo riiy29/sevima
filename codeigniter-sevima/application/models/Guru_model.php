a7aa44ee32ff81db084f6947946e278e9b02f7a6
Buchori <muhammad.buchori13@gmail.com>
2022-06-26 01:41:42 +0700

    testupdate

--- codeigniter-sevima/application/models/Guru_model.php
+++ codeigniter-sevima/application/models/Guru_model.php
@@ -7,7 +7,7 @@ class Guru_model extends CI_Model {
     {
         $this->db->select('*');
         $this->db->from('sevima_guru');
-        $this->db->join('sevima_jurusan','sevima_guru.id_jurusan = sevima_guru.id_jurusan', 'LEFT');
+        $this->db->join('sevima_jurusan','sevima_jurusan.id_jurusan = sevima_guru.id_jurusan', 'LEFT');
         $this->db->join('sevima_user','sevima_jurusan.hak_akses = sevima_user.hak_akses', 'LEFT');     
         $query = $this->db->get();
         return $query->result();
